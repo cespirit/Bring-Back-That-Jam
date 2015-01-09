28631b1f261f371fc74c156304da531fa8644edc
cespirit <ckespir@gmail.com>
2015-01-08 23:58:36 -0800

    Get back json data

---
+++ js/app.js
@@ -0,0 +1,73 @@
+$(document).ready(function(){
+
+	var jamsPerPage = 3;
+
+	$("#userForm").submit(function(event){
+		event.preventDefault();
+		var username = $(this).find("input[name='username']").val();
+		username = $.trim(username);
+		if(!isValidInput(username)) {
+			alert("Please fill in a username.");
+			return;
+		}
+
+		resetResultFields();
+		getPastJams(username);				
+	});
+
+	var isValidInput = function(username) {
+		if(username === "") {
+			return false;
+		}
+		return true;
+	};
+
+	var resetResultFields = function() {
+		$("#userInput").val("");
+		$("#jamResults").html("");
+	};
+
+	var getPastJams = function(username) {
+
+		console.log("-----------------");
+
+		var request = { 
+			show: "past",
+			key: "1155729500c504209f43e65fd110766512213181"
+		};
+
+		var results = $.ajax({
+			url: "http://api.thisismyjam.com/1/" + username + "/jams.json",
+			data: request,
+			dataType: "json",
+			type: "GET",
+			crossDomain: "true"
+		})
+		.done(function(result){
+			console.log(result);
+		})
+		.fail(function(jqXHR, error, errorThrown){
+			if(jqXHR.status == 404) {
+				alert("Username " + username + " does not exist.");				
+			}
+			else {
+				alert("An error occurred. Please try again later.");
+			}
+			return;
+		});
+
+		displayPastJams(username);
+	};
+
+	var displayPastJams = function(username) {		
+
+		var html = "<h2><a href='http://www.thisismyjam.com/" + username + "'>" + username + "</a> jams</h2>";		
+		$("#jamResults").html(html);	
+
+		/* TODO: Handle no past jams */
+
+		/* TODO: Handle past jams */
+
+	};
+
+});
